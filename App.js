fc1a82871b2566da3f10d5e5999fe412984a680e
Meagan <meagan@bowtie.co>
2019-02-28 11:06:48 -0700

    Add fontawesome icon and async asset loading

--- App.js
+++ App.js
@@ -1,28 +1,48 @@
 import React from 'react';
 import {  StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Font } from 'expo';
 
 import FooterTabNavigator from './navigation/FooterTabNavigator';
 
 export default class App extends React.Component {
-  constructor(props){
-    super(props);
+
+  state= {
+    isLoadingComplete: false,
   }
-  loadAssetsAsync = async () => {
-    await Font.loadAsync({
-      'FontAwesome': require('./assets/fonts/fontawesome.ttf')
-    })
-  }
-  componentDidMount() {
-    this.loadAssetsAsync()
+
+  _loadAssetsAsync = async () => {
+    return Promise.all([
+      Font.loadAsync({
+        'FontAwesome5FreeSolid': require('./assets/fonts/fontawesome.ttf')
+      })
+    ]);
+  };
+
+  _handleLoadingError = error => {
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar barStyle="light-content"/>
-        <FooterTabNavigator />
-      </View>
-    );
+    if(!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadAssetsAsync}
+          onError ={this._handleLoadingError}
+          onFinish ={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <StatusBar barStyle="light-content"/>
+          <FooterTabNavigator />
+        </View>
+      );
+    }
   }
 }
 
